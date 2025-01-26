75d6bb5bb5fb73c7d0a7f746d27c4f273376ea9c
Genie16-github <4411dlwoqja@naver.com>
2025-01-27 01:56:30 +0900

    벌집

--- Mathematics/B2292.java
+++ Mathematics/B2292.java
@@ -1,4 +1,24 @@
 package Mathematics;
 
+import java.util.Scanner;
+
 public class B2292 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        int cnt = 1;
+        int r = 2;
+
+        if (N == 1) {
+            System.out.println(1);
+        }
+        else {
+            while (r <= N) {
+                r = r + (6 * cnt);
+                cnt++;
+            }
+            System.out.println(cnt);
+        }
+    }
 }
